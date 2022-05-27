587e89731bd657b259afa6a420d6de27429456f2
Aman <77211729+amanisreal@users.noreply.github.com>
2022-05-28 02:12:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from "./Components/Header/Header"
+import Nav from "./Components/Nav/Nav"
+import About from "./Components/About/About"
+import Experience from "./Components/Experience/Experience"
+import Services from "./Components/Services/Services"
 
-function App() {
+import Contact from "./Components/Contact/Contact"
+import Footer from "./Components/Footer/Footer"
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+       <Header/> 
+       <Nav/>
+       <About/>
+       <Experience/>
+       <Services/>
+       <Footer/>
+
+    </>
+  )
 }
 
-export default App;
+export default App
